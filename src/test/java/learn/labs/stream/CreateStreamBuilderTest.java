79e3c9ee3b23c83edba0fa21b9d695fd9c5f1d7d
hadipsy27 <hadipsy27@gmail.com>
2023-08-13 11:33:44 +0700

    Stream: Stream Builder

---
+++ src/test/java/learn/labs/stream/CreateStreamBuilderTest.java
@@ -0,0 +1,30 @@
+package learn.labs.stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Objects;
+import java.util.stream.Stream;
+
+public class CreateStreamBuilderTest {
+
+    @Test
+    void testCreateStreamBuilder() {
+        Stream.Builder<String> builder = Stream.builder();
+        builder.accept("Hadi");
+        builder.add("Prasetyo").add("Dika");
+
+        Stream<String> stream = builder.build();
+        stream.forEach(System.out::println);
+    }
+
+    @Test
+    void testCreateStreamBuilderSimplify() {
+        Stream<Object> stream = Stream.builder()
+                .add("Hadi")
+                .add("Prasetyo")
+                .add("Dika")
+                .build();
+
+        stream.forEach(System.out::println);
+    }
+}
