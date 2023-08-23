429204b2c358edd60a2cf131a423ce76e987989a
hadipsy27 <hadipsy27@gmail.com>
2023-08-23 11:14:39 +0700

    Stream: Group By, Partition By Operation

---
+++ src/test/java/learn/labs/stream/GroupingByTest.java
@@ -0,0 +1,55 @@
+package learn.labs.stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class GroupingByTest {
+
+    @Test
+    void testGroupingBy() {
+        Map<String, List<Integer>> groupingBy = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+            .collect(Collectors.groupingBy(number -> {
+                if (number > 5) {
+                    return "Besar";
+                } else {
+                    return "Kecil";
+                }
+            }));
+        System.out.println(groupingBy);
+    }
+
+    @Test
+    void testPartitioningBy() {
+        Map<Boolean, List<Integer>> groupingBy = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+                .collect(Collectors.partitioningBy(number -> {
+                    return number > 5;
+                }));
+        System.out.println(groupingBy);
+    }
+
+    @Test
+    void testGroupingBy2() {
+        Map<String, List<String>> map = Stream.of("Hadi","Prasetyo","Dika","Kurniawan","Suryanto")
+            .collect(Collectors.groupingBy(name -> {
+                if(name.length() > 4){
+                    return "Panjang";
+                } else {
+                    return "Pendek";
+                }
+            }));
+        System.out.println(map);
+    }
+
+    @Test
+    void testPartitioningBy2() {
+        Map<Boolean, List<String>> map = Stream.of("Hadi","Prasetyo","Dika","Kurniawan","Suryanto")
+                .collect(Collectors.partitioningBy(name -> {
+                    return name.length() > 4;
+                }));
+        System.out.println(map);
+    }
+}
